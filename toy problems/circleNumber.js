24d060c8070532945095b1e54dd9ab8167a0df0e
andy <chaochaoqi@gmail.com>
2018-05-29 13:50:36 -0700

    circle nujm

---
+++ toy problems/circleNumber.js
@@ -0,0 +1,16 @@
+// Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighbouring numbers is equal (note that 0 and n - 1 are neighbouring, too).
+
+// Given n and firstNumber, find the number which is written in the radially opposite position to firstNumber.
+
+// Example
+
+// For n = 10 and firstNumber = 2, the output should be
+// circleOfNumbers(n, firstNumber) = 7
+
+function circleOfNumbers(n, firstNumber) {
+    let distance = Math.floor (n / 2);
+    if (firstNumber === distance) {
+        return 0;
+    }
+    return (firstNumber > distance) ? firstNumber - distance : firstNumber + distance;
+}
